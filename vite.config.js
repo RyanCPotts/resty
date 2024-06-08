4c7934db6e60d1be1fe71593d34ca861e0cf4f87
RYAN <ryancpotts@gmail.com>
2024-06-08 15:15:10 -0500

    lab-026 done

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests.config.js'],
+    testMatch: ['./src/**/*.test.jsx'],
+    globals: true
+  }
+});

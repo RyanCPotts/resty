e71f238cdd3df9c725a51eddb38762e62ff36064
RYAN <ryancpotts@gmail.com>
2024-06-10 16:30:44 -0500

    lab 27(28?)

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 import './Form.scss';
 
-class Form extends React.Component {
+const Form = (handleApiCall)=>{
 
-  handleSubmit = e => {
+  function handleSubmit(e) {
     e.preventDefault();
     const formData = {
       method:'GET',
       url: 'https://pokeapi.co/api/v2/pokemon',
     };
-    this.props.handleApiCall(formData);
+    handleApiCall(formData);
   }
-
-  render() {
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <label >
             <span>URL: </span>
             <input name='url' type='text' />
@@ -32,6 +30,5 @@ class Form extends React.Component {
       </>
     );
   }
-}
 
 export default Form;
